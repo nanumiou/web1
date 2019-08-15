0d503681c0c7ff194260c8274f8c8d7cf7a50b77
nanumiou <50026379+nanumiou@users.noreply.github.com>
2019-08-15 19:37:08 +0900

    Add files via upload

--- hidden.js
+++ hidden.js
@@ -1,4 +1,4 @@
-
+/*
 // 모바일 웹 주소창 숨기기
 window.addEventListener('load', function() {
   // body의 height를 살짝 늘리는 코드
@@ -6,3 +6,17 @@ window.addEventListener('load', function() {
   // scroll를 제어 하는 코드
   setTimeout(scrollTo, 0, 0, 1);
 }, false);
+*/
+
+// onload 이벤트
+window.onload = function() {
+    setTimeout(function() {
+        window.scrollTo(0, 1);}, 100);
+};
+
+// addEventListener 이벤트
+window.addEventListener('load', function(){
+    setTimeout(scrollTo, 1);
+}, false);
+
+
